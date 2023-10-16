e9c45ec2b88aa4327b5f5ec8b6196d44bd8e7e33
simone.leveghi <simone.leveghi@buonarroti.tn.it>
2023-10-16 08:29:53 +0200

    test: stampa di oggetto Persona

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Persona p = new Persona("Carlo", "Maschio", 1998);
+
+        System.out.println(p.toString());
     }
 }
