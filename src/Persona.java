b034c075e88ad3af080b3d9189088d1e356a989e
simone.leveghi <simone.leveghi@buonarroti.tn.it>
2023-10-16 08:25:04 +0200

    feat: metodi base

--- src/Persona.java
+++ src/Persona.java
@@ -1,2 +1,21 @@
+import java.util.Date;
+
 public class Persona {
+    private String nome;
+    private String sesso;
+    private int annoNascita;
+
+    public Persona(String nome, String sesso, int annoNascita) {
+        this.nome = nome;
+        this.sesso = sesso;
+        this.annoNascita = annoNascita;
+    }
+
+    public String toString() {
+        return "{" +
+                "\"nome\":\"" + this.nome + "\"," +
+                "\"sesso\":\"" + this.sesso + "\"," +
+                "\"annoNascita\":" + this.annoNascita +
+                "}";
+    }
 }
