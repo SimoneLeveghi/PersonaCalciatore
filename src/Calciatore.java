40dd79951d1db00af93412bb85ce78a7ce4a52eb
simone.leveghi <simone.leveghi@buonarroti.tn.it>
2023-10-16 08:51:56 +0200

    feat: costruttore

--- src/Calciatore.java
+++ src/Calciatore.java
@@ -1,3 +1,15 @@
 public class Calciatore extends Persona {
+    private String ruolo;
+    private int numeroMaglia;
+
+    public Calciatore(
+            String nome, String sesso, int annoNascita,
+            String ruolo, int numeroMaglia
+    ) {
+        super(nome, sesso, annoNascita);
+        this.ruolo = ruolo;
+        this.numeroMaglia = numeroMaglia;
+    }
+
 
 }
