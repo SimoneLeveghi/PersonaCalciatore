adb3f2eec4b0f0e9712f53deb585c44b3027f947
simone.leveghi <simone.leveghi@buonarroti.tn.it>
2023-10-16 08:42:23 +0200

    test: metodo clone

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Persona p = new Persona("Carlo", "Maschio", 1998);
-
         System.out.println(p.toString());
+
+        Persona p2 = (Persona) p.clone();
+        System.out.println(p2.toString());
     }
 }
